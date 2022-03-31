a7bf046b4f4386381344c30091414d8fb0dd634d
Bizotto <bizotto.nicolas@gmail.com>
2022-03-31 12:15:57 -0300

    Adçoes

--- src/components/Minefideld.js
+++ src/components/Minefideld.js
@@ -4,7 +4,19 @@ import Field from './Field';
 
 export default props => {
   const rows = props.board.map((row, r) => {
-    const columns = props.board.map((field, c));
-    return <Field {...field} key={c} />;
+    /*Array de jsx*/
+    const columns = props.board.map((field, c) => {
+      return <Field {...field} key={c} />;
+    });
+    return  <View key={r}>{columns}</View>
   });
+  return <View style={styles.container}>{rows}</View>
 };
+
+
+const styles = StyleSheet.create({
+
+  container:{
+
+  }
+})
